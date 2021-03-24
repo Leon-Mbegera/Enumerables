cfbf71e79ccd684db3616b272353c20b89f41286
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-24 16:23:22 +0300

    custom my_each_with_index

--- enumerable.rb
+++ enumerable.rb
@@ -7,8 +7,30 @@ module Enumerable
       yield element
     end
   end 
+
+
+  def my_each_with_index
+    return to_enum unless block_given?
+    
+    array = to_a
+    array.length.times {|element| yield array[element], element}
   
+  end
+  
+
+
+
+
+
+
+
+
+
+
 end
 
 
-puts [3, 5, 8, 1].my_each 
+
+
+[3,5,8,1].my_each_with_index{|element, i| p "#{element} #{i}"}
+ 
