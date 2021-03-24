eb5b355caae3a89d6a1ffdac2ff870c209fae9ce
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-24 18:10:25 +0300

    custom my_count

--- enumerable.rb
+++ enumerable.rb
@@ -51,16 +51,47 @@ module Enumerable
   def my_none? (param = nil?)
     if block_given?
       to_a.my_each{|i| return false if yield i}
-      elsif param 
+    elsif param 
       to_a.my_each {|i| return false if match?(i, param)}
-      else
+    else
       to_a.my_each {|i| return false if i}
     end
     true
   end
 
+  def my_count(param = nil)
+    count =  0
+    if block_given?
+      my_each{|i| count += 1 if yield i}
+    elsif param
+      my_each{|i| count += 1 if match?(i, param)}
+    else
+    return length
+    end
+    count
+  end
+
   
+
+
+  def match?(i, param)
+    case param
+    when Regexp
+      i =~ param
+    when Class
+      i.is_a?(param)
+    else
+      i == param
+    end
+  end
+
+
 end
 
+arr = [1, 2, 4, 2]
+puts (arr.my_count { |x| (x % 2).zero? })
+puts arr.my_count #=> 4
+puts arr.my_count(2) #=> 2
+ #=> 3\
 
 
