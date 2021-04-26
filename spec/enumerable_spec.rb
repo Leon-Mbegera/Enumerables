da7b0fa13e98aa3adfa4cbbb5ea37ad3885e2b26
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-04-26 13:20:23 +0300

    Create contant variables to use in test cases

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,2 +1,10 @@
 require "../enumerable.rb"
 
+describe 'Enumerables'
+
+  let(:arr) {[1, 2, 3, 4, 5]}
+  let(:neg_arr) {[1, 2, -3, 4]}
+  let(:alike_arr) {[7, 7, 7]}
+  let(:empty_arr) {[]}
+  let(:false_arr) {[nil, false]}
+end
